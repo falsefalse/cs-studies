d8b525c2b3f4eeba5d6e2714f9b3f07389dc6f67
Ilya Furman <smashlong@gmail.com>
2013-08-07 23:01:50 +0300

    4 spaces

--- sort_tests.js
+++ sort_tests.js
@@ -3,39 +3,39 @@
 var Sort = require('./sort.js');
 
 function shuffle(array) {
-  var shuffled = [], rand, i, value;
-  for (i = 0; i < array.length; i++) {
-    value = array[i];
-    if (i == 0) {
-      shuffled[0] = value;
-    } else {
-      rand = Math.floor(Math.random() * (i + 1));
-      shuffled[i] = shuffled[rand];
-      shuffled[rand] = value;
+    var shuffled = [], rand, i, value;
+    for (i = 0; i < array.length; i++) {
+        value = array[i];
+        if (i == 0) {
+            shuffled[0] = value;
+        } else {
+            rand = Math.floor(Math.random() * (i + 1));
+            shuffled[i] = shuffled[rand];
+            shuffled[rand] = value;
+        }
     }
-  }
-  return shuffled;
+    return shuffled;
 };
 
 var a, b, c, d, s = [], l = [];
 function _fill () {
-  a = [88, 8,7,6,5,4,3,2,1];
-  b = [1,2,3,4,5];
-  c = [0,1,3];
-  d = [8,7,6,5,4,3,14];
+    a = [88, 8,70,0,0,6,5,4,3,2,1];
+    b = [1,2,3,4,5];
+    c = [0,1,3];
+    d = [8,7,6,5,4,3,14];
 
-  s = shuffle(a);
+    s = shuffle(a);
 
-  l = []
-    .concat( shuffle(a) )
-    .concat( shuffle(a) )
-    .concat( shuffle(b) )
-    .concat( shuffle(b) )
-    .concat( shuffle(c) )
-    .concat( shuffle(c) )
-    .concat( shuffle(d) )
-    .concat( shuffle(d) )
-  ;
+    l = []
+        .concat( shuffle(a) )
+        .concat( shuffle(a) )
+        .concat( shuffle(b) )
+        .concat( shuffle(b) )
+        .concat( shuffle(c) )
+        .concat( shuffle(c) )
+        .concat( shuffle(d) )
+        .concat( shuffle(d) )
+    ;
 }
 
 console.log('== Merge sort')
